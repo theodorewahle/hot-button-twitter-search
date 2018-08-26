0c5f3b89ba6efe70eea441c5892304d169832156
“theodorewahle” <theodorewahle@gmail.com>
2018-08-25 22:27:27 -0400

    got twtitter feed component up and running

---
+++ hot-button-frontend/src/components/TwitterFeed.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import TweetEmbed from 'react-tweet-embed'
+
+const TwitterFeed = ({ tweets }) => {
+  const temp_tweets = {
+    "0": "1032256443985084417",
+    "1": "1033147164812300288",
+    "2": "1032736867391811584",
+    "3": "1033339636343549952",
+    "4": "1032431371728445441",
+    "5": "1032260490439864320", "6": "1032428929980739585", "7": "1033327353227882496", "8": "1033152062203342850", "9": "1032282621290704897"}
+  const tweet_ids = Object.keys(temp_tweets).map(key => temp_tweets[key])
+  return (
+    <div>
+    {tweet_ids.map(id => <TweetEmbed  id={id}/>)}
+    </div>
+  )
+}
+
+export default TwitterFeed
