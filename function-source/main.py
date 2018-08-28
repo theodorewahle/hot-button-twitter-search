f0ffbc0f0c8385cd1a544c9c87698abd336f7bc5
“theodorewahle” <theodorewahle@gmail.com>
2018-08-28 16:13:20 -0400

    Fixed the cloud function source code to enable CORS

--- function-source/main.py
+++ function-source/main.py
@@ -1,8 +1,18 @@
 import json
+from flask import make_response, jsonify
 from learning import get_top_tweets
 
 def top_tweets(request):
+    if request.method == "OPTIONS":
+        response = make_response(jsonify({ "message" : "success" }), 204)
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = 'Content-Type'
+        return response
     request_json = request.get_json()
     if request_json and 'user' in request_json:
         user = request_json['user']
-        return json.dumps({ "tweets" : get_top_tweets(user)})
+        tweets = { "tweets" : get_top_tweets(user)}
+        response = make_response(jsonify(tweets), 200)
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = 'Content-Type'
+        return response
--- hot-button-frontend/src/redux/tweets/Api.js
+++ hot-button-frontend/src/redux/tweets/Api.js
@@ -5,7 +5,12 @@ export const getTweets = user => async dispatch => {
   try {
     dispatch({ type: GET_TWEETS });
     const url = "https://us-east1-hot-button-twitter-search.cloudfunctions.net/sorted-tweets-1"
-    const response = await axios.post(url, {user});
+    const request = {
+      method: 'POST',
+      url,
+      data: { user },
+    }
+    const response = await axios(request);
     dispatch(getTweetsSuccess({ user, tweets: response.data.tweets }));
   } catch (e) {
     console.log(e);
